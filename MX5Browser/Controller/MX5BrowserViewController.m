9f021e13e86dd2b58e005806ef53075f1ee2aac3
kingly <libintm@163.com>
2017-11-10 13:08:19 +0800

    初始化工程

---
+++ MX5Browser/Controller/MX5BrowserViewController.m
@@ -0,0 +1,56 @@
+//
+//  MX5BrowserViewController.m
+//  MX5BrowserOC
+//
+//  Created by kingly on 2017/11/10.
+//  Copyright © 2017年 MX5Browser Software https://github.com/kingly09/MX5Browser  by kingly inc.  
+
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE. All rights reserved.
+//
+
+#import "MX5BrowserViewController.h"
+
+@interface MX5BrowserViewController ()
+
+@end
+
+@implementation MX5BrowserViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
---
+++ MX5Browser/MX5Browser.h
@@ -0,0 +1,67 @@
+//
+//  MX5Browser.h
+//  MX5BrowserOC
+//
+//  Created by kingly on 2017/11/10.
+//  Copyright © 2017年 MX5Browser Software https://github.com/kingly09/MX5Browser  by kingly inc.  
+
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE. All rights reserved.
+//
+
+#ifndef MX5Browser_h
+#define MX5Browser_h
+
+#import "MX5WebView.h"
+
+
+#pragma mark - WEAK、STRONG
+
+//weak、strong创建
+#define WEAK_REF(self) \
+__block __weak typeof(self) self##_ = self; (void) self##_;
+
+#define STRONG_REF(self) \
+__block __strong typeof(self) self##_ = self; (void) self##_;
+
+#pragma mark - SharedInstance
+
+#define SYNTHESIZE_SINGLETON_FOR_CLASS_HEADER(__CLASSNAME__)    \
+\
++ (__CLASSNAME__*) sharedInstance;
+
+
+#define SYNTHESIZE_SINGLETON_FOR_CLASS(__CLASSNAME__)    \
++ (__CLASSNAME__ *)sharedInstance\
+{\
+static __CLASSNAME__ *shared##className = nil;\
+static dispatch_once_t onceToken; \
+dispatch_once(&onceToken, ^{ \
+shared##className = [[super allocWithZone:NULL] init]; \
+}); \
+return shared##className; \
+}\
++ (id)allocWithZone:(NSZone*)zone {\
+return [self sharedInstance];\
+}\
+- (id)copyWithZone:(NSZone *)zone {\
+return self;\
+}
+
+#endif /* MX5Browser_h */
