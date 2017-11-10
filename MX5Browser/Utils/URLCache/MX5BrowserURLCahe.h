9ac3a52349a135fdb38c50dfb4e41e8c9f86f03b
kingly <libintm@163.com>
2017-11-10 14:58:23 +0800

    删除 文件

--- MX5Browser/Utils/URLCache/MX5BrowserURLCahe.h
+++ MX5Browser/Utils/URLCache/MX5BrowserURLCahe.h
@@ -27,6 +27,15 @@
 
 #import <Foundation/Foundation.h>
 
-@interface MX5BrowserURLCahe : NSObject
+/**
+ 浏览器URL
+ */
+@interface MX5BrowserURLCahe : NSURLCache
+
+@property(nonatomic, assign) NSInteger cacheTime;
+@property(nonatomic, retain) NSString *diskPath;
+@property(nonatomic, retain) NSMutableDictionary *responseDictionary;
+
+- (id)initWithMemoryCapacity:(NSUInteger)memoryCapacity diskCapacity:(NSUInteger)diskCapacity diskPath:(NSString *)path cacheTime:(NSInteger)cacheTime;
 
 @end
